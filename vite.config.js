6670cc8861585f9050e299966081f4feac100f84
allen <thegreatcosmoslight@gmail.com>
2026-02-26 23:48:54 +0800

    set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/AutoCombo/',   // ★ 改成 GitHub repo 名
 })
