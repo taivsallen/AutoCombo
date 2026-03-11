50aaa22fedf51b19295b94d76f3c05dfc3c81831
allen <thegreatcosmoslight@gmail.com>
2026-03-11 13:27:54 +0800

    add more fix broad

--- src/activeSkillTemplateData.jsx
+++ src/activeSkillTemplateData.jsx
@@ -72,7 +72,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11130",
     characterName: "悠久繽紛 ‧ 烏瑞亞",
-	note: "(回合結束時)",
+	note: "沒發動角色珠及回合結束時",
     characterImg: "./characters/11130.png",
     attribute: ATTRIBUTES.PLANT,
     race: RACES.GOD,
@@ -134,6 +134,22 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
 			],
   },
   {
+    id: "11117",
+    characterName: "傳情玫瑰 ‧ 梅蘭妮",
+	note: "開技時及回合結束時",
+    characterImg: "./characters/11117.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.MACHINA,
+	board:
+			[
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h'
+			],
+  },
+  {
     id: "11113",
     characterName: "蟻王",
 	note: "",
@@ -152,7 +168,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11108",
     characterName: "血腥伊格利特",
-	note: "",
+	note: "開技時及回合結束時",
     characterImg: "./characters/11108.png",
     attribute: ATTRIBUTES.FIRE,
     race: RACES.DEMON,
@@ -168,7 +184,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11096",
     characterName: "闇影君主 ‧ 成振宇",
-	note: "",
+	note: "【伊格利特】、開技時及回合結束時",
     characterImg: "./characters/11096.png",
     attribute: ATTRIBUTES.DARK,
     race: RACES.HUMAN,
@@ -198,6 +214,22 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
 			],
   },
   {
+    id: "11093",
+    characterName: "崔鐘仁",
+	note: "",
+    characterImg: "./characters/11093.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'f', 'f', 'f', 'f', 'f', 'f'
+			],
+  },
+  {
     id: "11080",
     characterName: "肌肉咖波",
 	note: "",
@@ -216,7 +248,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11079",
     characterName: "狗狗",
-	note: "(技能關閉時)",
+	note: "技能關閉時",
     characterImg: "./characters/11079.png",
     attribute: ATTRIBUTES.LIGHT,
     race: RACES.BEAST,
@@ -248,7 +280,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11066",
     characterName: "百計不休 ‧ 菅原道真",
-	note: "",
+	note: "開技時及回合結束時",
     characterImg: "./characters/11066.png",
     attribute: ATTRIBUTES.WATER,
     race: RACES.DEMON,
@@ -264,7 +296,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11055",
     characterName: "凜潔心界 ‧ 曼陀羅",
-	note: "(【命運迴響】)",
+	note: "【命運迴響】",
     characterImg: "./characters/11055.png",
     attribute: ATTRIBUTES.FIRE,
     race: RACES.ELF,
@@ -280,7 +312,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11047",
     characterName: "神的信使 ‧ 吉列爾",
-	note: "",
+	note: "開技時及回合結束時",
     characterImg: "./characters/11047.png",
     attribute: ATTRIBUTES.LIGHT,
     race: RACES.GOD,
@@ -296,7 +328,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
     id: "11044",
     characterName: "銀色劍姬 ‧ 希露法",
-	note: "",
+	note: "開技時及回合結束時",
     characterImg: "./characters/11044.png",
     attribute: ATTRIBUTES.DARK,
     race: RACES.HUMAN,
@@ -326,9 +358,9 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
 			],
   },
   {
-    id: "11040",
+    id: "11040_a",
     characterName: "巨鼠 ‧ 巴比倫",
-	note: "",
+	note: "開技時",
     characterImg: "./characters/11040.png",
     attribute: ATTRIBUTES.PLANT,
     race: RACES.HUMAN,
@@ -339,6 +371,22 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
 				'h', 'p', 'h', 'p', 'h', 'p', 
 				'h', 'p', 'p', 'p', 'h', 'p', 
 				'h', 'h', 'h', 'h', 'h', 'p'
+			],
+  },
+  {
+    id: "11040_b",
+    characterName: "巨鼠 ‧ 巴比倫",
+	note: "回合結束時",
+    characterImg: "./characters/11040.png",
+    attribute: ATTRIBUTES.PLANT,
+    race: RACES.HUMAN,
+	board:
+			[
+				'p', 'h', 'h', 'h', 'h', 'h', 
+				'p', 'h', 'p', 'p', 'p', 'h', 
+				'p', 'h', 'p', 'h', 'p', 'h', 
+				'p', 'h', 'h', 'h', 'p', 'h', 
+				'p', 'p', 'p', 'p', 'p', 'h'
 			],
   },
   {
@@ -408,7 +456,7 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
   {
   id: "10983",
     characterName: "黎星刻與神虎",
-	note: "",
+	note: "開技時及回合結束時",
     characterImg: "./characters/10983.png",
     attribute: ATTRIBUTES.WATER,
     race: RACES.MACHINA,
@@ -435,6 +483,390 @@ export const ACTIVE_SKILL_TEMPLATE_DATA = [
 				'f', 'l', 'f', 'l', 'f', 'l', 
 				'f', 'l', 'f', 'l', 'f', 'l', 
 				'f', 'l', 'f', 'l', 'f', 'l'
+			],
+  },
+  {
+  id: "10980_a",
+    characterName: "魯路修 ‧ 維 ‧ 不列顛尼亞與海市蜃樓",
+	note: "【戰鬥機】",
+    characterImg: "./characters/10980.png",
+    attribute: ATTRIBUTES.DARK,
+    race: RACES.MACHINA,
+	board:
+			[
+				'd', 'p', 'f', 'l', 'w', 'h', 
+				'd', 'p', 'f', 'l', 'w', 'h', 
+				'd', 'p', 'f', 'l', 'w', 'h', 
+				'd', 'p', 'f', 'l', 'w', 'h', 
+				'd', 'p', 'f', 'l', 'w', 'h'
+			],
+  },
+  {
+  id: "10980_b",
+    characterName: "魯路修 ‧ 維 ‧ 不列顛尼亞與海市蜃樓",
+	note: "【機甲】",
+    characterImg: "./characters/10980.png",
+    attribute: ATTRIBUTES.DARK,
+    race: RACES.MACHINA,
+	board:
+			[
+				'd', 'd', 'd', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'd', 'd', 'd', 
+				'd', 'd', 'd', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'd', 'd', 'd', 
+				'd', 'd', 'd', 'h', 'h', 'h'
+			],
+  },
+  {
+  id: "10979",
+    characterName: "傑瑞米亞 ‧ 哥德巴爾德與齊格菲",
+	note: "",
+    characterImg: "./characters/10979.png",
+    attribute: ATTRIBUTES.LIGHT,
+    race: RACES.MACHINA,
+	board:
+			[
+				'l', 'l', 'l', 'l', 'l', 'l', 
+				'l', 'w', 'f', 'p', 'd', 'l', 
+				'l', 'w', 'f', 'p', 'd', 'l', 
+				'l', 'w', 'f', 'p', 'd', 'l', 
+				'l', 'l', 'l', 'l', 'l', 'l'
+			],
+  },
+  {
+  id: "10977_a",
+    characterName: "柯內莉亞 ‧ 利 ‧ 不列顛尼亞與格洛斯特 (柯內莉亞機)",
+	note: "自身位於左方 3 直行",
+    characterImg: "./characters/10977.png",
+    attribute: ATTRIBUTES.DARK,
+    race: RACES.MACHINA,
+	board:
+			[
+				'p', 'p', 'p', 'p', 'p', 'p', 
+				'p', 'f', 'f', 'f', 'f', 'p', 
+				'p', 'f', 'w', 'w', 'f', 'p', 
+				'p', 'f', 'w', 'w', 'f', 'p', 
+				'p', 'f', 'w', 'w', 'f', 'p'
+			],
+  },
+  {
+  id: "10977_b",
+    characterName: "柯內莉亞 ‧ 利 ‧ 不列顛尼亞與格洛斯特 (柯內莉亞機)",
+	note: "自身位於右方 3 直行",
+    characterImg: "./characters/10977.png",
+    attribute: ATTRIBUTES.DARK,
+    race: RACES.MACHINA,
+	board:
+			[
+				'd', 'd', 'd', 'd', 'd', 'd', 
+				'd', 'l', 'l', 'l', 'l', 'd', 
+				'd', 'l', 'h', 'h', 'l', 'd', 
+				'd', 'l', 'h', 'h', 'l', 'd', 
+				'd', 'l', 'h', 'h', 'l', 'd'
+			],
+  },
+  {
+  id: "10969",
+    characterName: "尤菲米亞 ‧ 利 ‧ 不列顛尼亞",
+	note: "",
+    characterImg: "./characters/10969.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.HUMAN,
+	board:
+			[
+				'w', 'w', 'w', 'w', 'w', 'w', 
+				'w', 'f', 'p', 'l', 'd', 'h', 
+				'w', 'f', 'p', 'l', 'd', 'h', 
+				'w', 'f', 'p', 'l', 'd', 'h', 
+				'w', 'f', 'p', 'l', 'd', 'h'
+			],
+  },
+  {
+  id: "10962",
+    characterName: "碧藍假期 ‧ 阿圖姆",
+	note: "",
+    characterImg: "./characters/10962.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.MACHINA,
+	board:
+			[
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'f', 'l', 'l', 'l', 'l', 'f', 
+				'f', 'l', 'l', 'l', 'l', 'f', 
+				'f', 'l', 'l', 'l', 'l', 'f', 
+				'f', 'f', 'f', 'f', 'f', 'f'
+			],
+  },
+  {
+  id: "10953",
+    characterName: "妖精王 ‧ 奧伯隆",
+	note: "【緹坦妮雅】",
+    characterImg: "./characters/10953.png",
+    attribute: ATTRIBUTES.LIGHT,
+    race: RACES.ELF,
+	board:
+			[
+				'l', 'l', 'l', 'w', 'w', 'w', 
+				'l', 'f', 'f', 'f', 'f', 'w', 
+				'l', 'p', 'p', 'p', 'p', 'w', 
+				'l', 'd', 'd', 'd', 'd', 'w', 
+				'l', 'l', 'l', 'w', 'w', 'w'
+			],
+  },
+  {
+  id: "10950",
+    characterName: "五宿思慕 ‧ 緹坦妮雅",
+	note: "開技時及每回合結束時",
+    characterImg: "./characters/10950.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.ELF,
+	board:
+			[
+				'w', 'w', 'w', 'w', 'w', 'w', 
+				'f', 'p', 'l', 'd', 'h', 'w', 
+				'f', 'p', 'l', 'd', 'h', 'w', 
+				'f', 'p', 'l', 'd', 'h', 'w', 
+				'f', 'p', 'l', 'd', 'h', 'w'
+			],
+  },
+  {
+  id: "10947",
+    characterName: "鞘姬 (黑川茜)",
+	note: "關閉技能時",
+    characterImg: "./characters/10947.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.DEMON,
+	board:
+			[
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'h', 'h', 'h'
+			],
+  },
+  {
+  id: "10943",
+    characterName: "刀 (姬川大輝)",
+	note: "",
+    characterImg: "./characters/10943.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.DEMON,
+	board:
+			[
+				'w', 'f', 'f', 'f', 'f', 'w', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'h', 'h', 'h', 'h', 'h', 'h', 
+				'w', 'f', 'f', 'f', 'f', 'w'
+			],
+  },
+  {
+  id: "10941",
+    characterName: "皮耶勇酷雞",
+	note: "",
+    characterImg: "./characters/10941.png",
+    attribute: ATTRIBUTES.LIGHT,
+    race: RACES.HUMAN,
+	board:
+			[
+				'h', 'h', 'l', 'l', 'h', 'h', 
+				'h', 'l', 'l', 'l', 'l', 'h', 
+				'l', 'l', 'l', 'l', 'l', 'l', 
+				'h', 'l', 'l', 'l', 'l', 'h', 
+				'h', 'h', 'l', 'l', 'h', 'h'
+			],
+  },
+  {
+  id: "10939",
+    characterName: "B小町",
+	note: "合體卡",
+    characterImg: "./characters/10939.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'f', 'f', 'f', 'f', 'h', 
+				'f', 'w', 'p', 'l', 'd', 'h', 
+				'f', 'w', 'p', 'l', 'd', 'h', 
+				'f', 'w', 'p', 'l', 'd', 'h', 
+				'f', 'h', 'h', 'h', 'h', 'h'
+			],
+  },
+  {
+  id: "10938_a",
+    characterName: "B小町 ‧ MEMcho",
+	note: "",
+    characterImg: "./characters/10938.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'l', 'l', 'l', 'l', 'f', 
+				'f', 'f', 'l', 'l', 'f', 'f', 
+				'f', 'l', 'f', 'f', 'l', 'f', 
+				'f', 'l', 'l', 'l', 'l', 'f', 
+				'f', 'l', 'l', 'l', 'l', 'f'
+			],
+  },
+  {
+  id: "10938_b",
+    characterName: "B小町 ‧ MEMcho",
+	note: "合體時",
+    characterImg: "./characters/10938.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l'
+			],
+  },
+  {
+  id: "10936",
+    characterName: "B小町 ‧ 有馬加奈",
+	note: "合體時",
+    characterImg: "./characters/10936.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l'
+			],
+  },
+  {
+  id: "10934",
+    characterName: "MEMcho",
+	note: "",
+    characterImg: "./characters/10934.png",
+    attribute: ATTRIBUTES.LIGHT,
+    race: RACES.HUMAN,
+	board:
+			[
+				'l', 'h', 'h', 'h', 'h', 'l', 
+				'l', 'l', 'h', 'h', 'l', 'l', 
+				'l', 'h', 'l', 'l', 'h', 'l', 
+				'l', 'h', 'h', 'h', 'h', 'l', 
+				'l', 'h', 'h', 'h', 'h', 'l'
+			],
+  },
+  {
+  id: "10932",
+    characterName: "B小町 ‧ 露比",
+	note: "合體時",
+    characterImg: "./characters/10932.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.HUMAN,
+	board:
+			[
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l', 
+				'f', 'h', 'l', 'f', 'h', 'l'
+			],
+  },
+  {
+  id: "10931_a",
+    characterName: "最佳應援 ‧ 阿奎亞",
+	note: "最上面",
+    characterImg: "./characters/10931.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.HUMAN,
+	board:
+			[
+				'w', 'f', 'p', 'w', 'f', 'p', 
+				'w', 'f', 'p', 'w', 'f', 'p', 
+				'w', 'f', 'p', 'w', 'f', 'p', 
+				'w', 'f', 'p', 'w', 'f', 'p', 
+				'w', 'f', 'p', 'w', 'f', 'p'
+			],
+  },
+  {
+  id: "10931_b",
+    characterName: "最佳應援 ‧ 阿奎亞",
+	note: "上往下第二",
+    characterImg: "./characters/10931.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.HUMAN,
+	board:
+			[
+				'w', 'w', 'w', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'w', 'w', 'w', 
+				'w', 'w', 'w', 'h', 'h', 'h', 
+				'h', 'h', 'h', 'w', 'w', 'w', 
+				'w', 'w', 'w', 'h', 'h', 'h'
+			],
+  },
+  {
+  id: "10931_c",
+    characterName: "最佳應援 ‧ 阿奎亞",
+	note: "上往下第三",
+    characterImg: "./characters/10931.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.HUMAN,
+	board:
+			[
+				'l', 'd', 'h', 'l', 'd', 'h', 
+				'l', 'd', 'h', 'l', 'd', 'h', 
+				'l', 'd', 'h', 'l', 'd', 'h', 
+				'l', 'd', 'h', 'l', 'd', 'h', 
+				'l', 'd', 'h', 'l', 'd', 'h'
+			],
+  },
+  {
+  id: "10931_d",
+    characterName: "最佳應援 ‧ 阿奎亞",
+	note: "有小愛時，最下面",
+    characterImg: "./characters/10931.png",
+    attribute: ATTRIBUTES.WATER,
+    race: RACES.HUMAN,
+	board:
+			[
+				'w', 'w', 'w', 'd', 'd', 'd', 
+				'h', 'f', 'p', 'w', 'l', 'h', 
+				'h', 'f', 'p', 'w', 'l', 'h', 
+				'h', 'f', 'p', 'w', 'l', 'h', 
+				'd', 'd', 'd', 'f', 'f', 'f'
+			],
+  },
+  {
+  id: "10921_a",
+    characterName: "侵掠效應 ‧ 麥科洛",
+	note: "開技時",
+    characterImg: "./characters/10921.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.DEMON,
+	board:
+			[
+				'f', 'p', 'f', 'p', 'f', 'p', 
+				'f', 'p', 'f', 'p', 'f', 'p', 
+				'f', 'p', 'f', 'p', 'f', 'p', 
+				'f', 'p', 'f', 'p', 'f', 'p', 
+				'f', 'p', 'f', 'p', 'f', 'p'
+			],
+  },
+  {
+  id: "10921_b",
+    characterName: "侵掠效應 ‧ 麥科洛",
+	note: "回合結束時",
+    characterImg: "./characters/10921.png",
+    attribute: ATTRIBUTES.FIRE,
+    race: RACES.DEMON,
+	board:
+			[
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'p', 'p', 'p', 'p', 'p', 'p', 
+				'f', 'f', 'f', 'f', 'f', 'f', 
+				'p', 'p', 'p', 'p', 'p', 'p', 
+				'f', 'f', 'f', 'f', 'f', 'f'
 			],
   },
 ];
